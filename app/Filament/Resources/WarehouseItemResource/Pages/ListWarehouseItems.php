3f1ca165e9afed0cb304bf3f553475b3c0cc7e47
alwizen <solu8i.project@gmail.com>
2025-05-18 22:39:39 +0700

    add tabs category warehouse

--- app/Filament/Resources/WarehouseItemResource/Pages/ListWarehouseItems.php
+++ app/Filament/Resources/WarehouseItemResource/Pages/ListWarehouseItems.php
@@ -4,7 +4,9 @@ namespace App\Filament\Resources\WarehouseItemResource\Pages;
 
 use App\Filament\Resources\WarehouseItemResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListWarehouseItems extends ListRecords
 {
@@ -16,4 +18,18 @@ class ListWarehouseItems extends ListRecords
             Actions\CreateAction::make(),
         ];
     }
+    public function getTabs(): array
+    {
+        return [
+            'semua' => Tab::make('Semua')
+                ->label('Semua Barang'),
+            'kering' => Tab::make('Kering')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('warehouse_category_id', 1)),
+            'basah' => Tab::make('Basah')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('warehouse_category_id', 2)),
+            'bumbu' => Tab::make('Bumbu')
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('warehouse_category_id', 3)),
+        ];
+    }
+
 }
